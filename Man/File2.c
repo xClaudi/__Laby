f950a6f68eacf5f97776c6519df4eaae05a6ede2
xClaudi <148841504+xClaudi@users.noreply.github.com>
2023-11-12 19:05:10 +0100

    Update File2.c

--- Man/File2.c
+++ Man/File2.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main()
+{
+	int liczba_calkowita;
+	printf("Podaj liczbe calkowita.\n");
+	scanf("%d", &liczba_calkowita);
+	if (liczba_calkowita % 2 == 0) {
+		printf("Podana liczba jest parzytsa.\n");
+	}
+	else
+	{
+		printf("Podana liczba nie jest parzysta.\n");
+
+	}
+}

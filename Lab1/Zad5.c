83d5492aba113bdbc7f0b4c9da770a8b2a89f033
xClaudi <148841504+xClaudi@users.noreply.github.com>
2023-11-30 20:42:22 +0100

    Update Zad5.c

--- Lab1/Zad5.c
+++ Lab1/Zad5.c
@@ -1,38 +1,36 @@
 #include <stdio.h>
 
 int main() {
-	int liczba1;
-	int liczba2;
+	float liczba1;
+	float liczba2;
+	char operator;
+
+	printf("Wybierz dzialanie ktore chcesz wykonac (+, -, *, /).\n");
+	scanf("%c", &operator);
 	printf("Podaj liczbe.\n");
-	scanf("%d", &liczba1);
+	scanf("%f", &liczba1);
 	printf("Podaj nastepna liczbe.\n");
-	scanf("%d", &liczba2);
-	printf("Jakie z podanych dzialan chcesz wykonac?.\n");
-	printf("1 - Dodawanie.\n");
-	printf("2 - Odejmowanie.\n");
-	printf("3 - Dzielenie.\n");
-	printf("4 - Mnozenie.\n");
-	int wybor;
-	scanf("%d", &wybor);
-	switch (wybor){
-	case 1:
-		printf("Wynik dodawania: %d.\n", liczba1 + liczba2);
+	scanf("%f", &liczba2);
+	
+
+
+	switch (operator)
+	{
+	case '+':
+		printf("%f + %f = %f\n", liczba1, liczba2, liczba1 + liczba2);
 		break;
-	case 2:
-		printf("Wynik odejmowania: %d.\n", liczba1 - liczba2);
+	case '-':
+		printf("%f - %f = %f\n", liczba1, liczba2, liczba1 - liczba2);
 		break;
-	case 3:
-		if (liczba2 != 0) {
-			printf("Wynik dzielenia: %f.\n", liczba1 / liczba2);
-		}
-		else {
-			printf("Dzielenie przez zero jest niedozwolone.\n");
-		}
+	case '*':
+		printf("%f * %f = %f\n", liczba1, liczba2, liczba1 * liczba2);
 		break;
-	case 4:
-		printf("Wynik mnozenia: %d", liczba1 * liczba2);
-		break;
+	case '/':
+		printf("%f / %f = %f\n", liczba1, liczba2, liczba1 / liczba2);
+
+
 	default:
-		printf("Wybrano niepoprawna opcje.\n");
+		printf("Nieprawidlowy znak\n");
+		break;
 	}
 }
